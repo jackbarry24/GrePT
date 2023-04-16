dbec126c2f1b0f375319260c22e5db030a3c7b79
Jack Barry <jack.barry@live.com>
2023-04-16 11:06:25 +0100

    bump version 1.0.2 -> 1.1.0

--- src/grept/__main__.py
+++ src/grept/__main__.py
@@ -4,7 +4,7 @@ from termcolor import colored, cprint
 from grept import completions
 import sys
 
-VERSION = "1.0.2"
+VERSION = "1.1.0"
 
 def _clear():
     os.system("cls" if os.name == "nt" else "clear")
