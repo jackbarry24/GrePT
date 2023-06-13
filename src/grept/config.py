a01804a687b4332b218929f7383cb8aeb8f44d3d
Jack Barry <jack.barry@live.com>
2023-06-13 12:46:30 -0700

    3.5-turbo-16k brrrrr

--- src/grept/config.py
+++ src/grept/config.py
@@ -1,7 +1,8 @@
-COMPLETIONS_MODEL = "gpt-3.5-turbo"
+COMPLETIONS_MODEL = "gpt-3.5-turbo-16k"
 EMBEDDINGS_MODEL = "text-embedding-ada-002"
 
 MAX_INPUT_TOKENS = {
+    "gpt-3.5-turbo-16k": 16384,
     "gpt-3.5-turbo": 4096, 
     "gpt-4": 8192, 
     "gpt-4-32k": 32768
